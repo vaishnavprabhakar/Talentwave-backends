865a518d7a1126f274fb0d1558d55a927724da34
Cybermate <vaishnavprabhakarkoo@gmail.com>
2023-11-14 20:26:16 +0530

    fix

--- authentication/managers.py
+++ authentication/managers.py
@@ -11,7 +11,7 @@ class CustomBaseUserManager(BaseUserManager):
         Create and save a user with the given email and password.
         """
         if not email:
-            raise ValueError(_("The Email must be set"))
+            raise ValueError(("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
